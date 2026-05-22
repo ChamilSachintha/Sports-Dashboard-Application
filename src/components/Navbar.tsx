94265b54de742123bd69023f4c5a46f245cf4724
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2026-05-22 15:57:25 +0530

    Change Navbar margins

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,10 +17,10 @@ export default function Navbar() {
 
   return (
     <nav className="bg-[#1d283b] border-b border-slate-800 text-slate-100 sticky top-0 z-50">
-      <div className="w-full mx-32 px-4 sm:px-6 lg:px-8">
+      <div className="w-full px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo & Nav Items */}
-          <div className="flex items-center space-x-8">
+          <div className="flex items-center space-x-8 ml-32">
             {/* LOGO */}
             <div className="flex items-center space-x-2">
               <img
@@ -49,7 +49,7 @@ export default function Navbar() {
           </div>
 
           {/* Action Icons & Sign In */}
-          <div className="hidden md:flex items-center space-x-4">
+          <div className="hidden md:flex items-center space-x-4 mr-32">
             {/* Search */}
             <button className="p-2 hover:bg-slate-800 rounded-lg transition">
               <img src={SearchIcon} alt="Search" className="w-5 h-5" />
