a62c8cf73c33c99a0d40195b9b96387e569978f2
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2026-05-22 14:43:49 +0530

    Add all assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-4xl font-bold text-center text-blue-600">
-        Testing
-      </h1>
-
-      
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <Hero />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,37 @@
+import HeroImg from "../assets/Hero.jpg";
+
+export default function HeroSection() {
+  return (
+    <section className="relative w-full h-[80vh] flex items-center text-left">
+
+      {/* Background Image */}
+      <div className="absolute inset-0">
+        <img
+          src={HeroImg}
+          alt="Hero"
+          className="w-full h-full object-cover"
+        />
+
+        {/* Dark Overlay */}
+        <div className="absolute inset-0 bg-black/60"></div>
+      </div>
+
+      {/* Content */}
+      <div className="relative z-10 text-white px-6 max-w-3xl">
+
+        <h1 className="text-3xl md:text-5xl font-extrabold leading-tight">
+          ICC Cricket World Cup 2025
+        </h1>
+
+        <p className="mt-4 text-base md:text-xl text-gray-200">
+          India vs Australia - Final Showdown
+        </p>
+
+        <button className="mt-6 bg-emerald-500 hover:bg-emerald-600 text-black font-bold px-6 py-3 rounded-xl transition">
+          Watch Now
+        </button>
+
+      </div>
+    </section>
+  );
+}
