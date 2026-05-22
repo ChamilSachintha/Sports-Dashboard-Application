a285f39cc785d7796f0d522e1165bf7dca4e87e4
Chamil Sachintha <87272440+ChamilSachintha@users.noreply.github.com>
2026-05-22 13:35:55 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-4xl font-bold text-center text-blue-600">
+        Testing
+      </h1>
+
+      
+    </div>
+  )
+}
+
+export default App
